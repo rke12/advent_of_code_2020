e6edc76a59a6285cd6e987d7b3e5f1c674efb74c
rory.kavanagh <rory.kavanagh@ucdconnect.ie>
2020-11-25 23:42:50 +0000

    Add day 4 instructions and starter code

---
+++ day-4-2019/one.js
@@ -0,0 +1,23 @@
+const passwordBreaker = (lowerLimit = 272091, upperLimit = 815432) => {
+    let possiblePasswords = [];
+
+    // Step 1: Loop through values between upperLimit and lowerLimit
+    let index = lowerLimit;
+    while (index <= upperLimit){
+        // Step 2
+
+        // Step 3
+
+        index++;
+    }
+
+    // Step 2: for each value in loop, figure out if values are descending
+    // if we have a match, add to the possiblePasswords array
+
+    // Step 3: for each value in loop, figure out if values hav adjacent pairs
+    // if we have a match, add to the possiblePasswords array
+
+    // Return length of possiblePasswords array
+    return possiblePasswords.length;
+}
+module.exports = passwordBreaker;
