acc45f6f99f87d18bf61d6e85c0ccda9a87ecef2
rory.kavanagh <rory.kavanagh@ucdconnect.ie>
2020-12-02 23:16:16 +0000

    Adding part 2 and tests

--- day-1-2020/test.js
+++ day-1-2020/test.js
@@ -1,9 +1,17 @@
 const assert = require('assert');
-const expenses = require('./one');
+const expensesTwo = require('./one');
+const expensesThree = require('./two');
 
-describe('Part One Intcode ', () => {
+describe('Part One expenseListTwo ', () => {
     it('should return 1010100 when input contains 1110 and 910', () => {
       let input = [1983, 387, 1193, 1110, 910];
-      assert.deepStrictEqual(expenses(input), 1010100);
+      assert.deepStrictEqual(expensesTwo(input), 1010100);
+    });
+});
+
+describe('Part Two expenseListThree ', () => {
+    it('should return 20000000 when input contains 1000, 1000 and 20', () => {
+      let input = [1289, 1092, 1000, 1000, 20];
+      assert.deepStrictEqual(expensesThree(input), 20000000);
     });
 });
